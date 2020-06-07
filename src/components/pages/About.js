3d97d193f18cc39ca134be8924cc2bc8fa99572f
Ujjwal Jha <ujjwal.jha@apexcollege.edu.np>
2020-06-07 18:42:14 +0545

    deploy my first react todo list app v1.0.0

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is my first Todo List app v1.0.0. using React. Thank you!</p>
+        </React.Fragment>
+    )
+}
+export default About;
