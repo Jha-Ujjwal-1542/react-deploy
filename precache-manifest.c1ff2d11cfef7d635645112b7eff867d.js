01293608a0e49efc521f3d049b36df1d79593f72
Ujjwal Jha <ujjwal.jha@apexcollege.edu.np>
2020-06-07 18:43:31 +0545

    Updates

---
+++ precache-manifest.c1ff2d11cfef7d635645112b7eff867d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df395fb0012465ea734cd6e3b8638679",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "7cd3e2afa28e7d36ed6d",
+    "url": "/react-deploy/static/css/main.667d2a23.chunk.css"
+  },
+  {
+    "revision": "05c7877ea576d225b456",
+    "url": "/react-deploy/static/js/2.294fdd8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy/static/js/2.294fdd8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd3e2afa28e7d36ed6d",
+    "url": "/react-deploy/static/js/main.7a8a9977.chunk.js"
+  },
+  {
+    "revision": "ed754a534f1bf06a256d",
+    "url": "/react-deploy/static/js/runtime-main.53f17c28.js"
+  }
+]);
